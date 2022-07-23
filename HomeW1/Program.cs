2bb06a9d1bda34f9e18311e299274234acce9c4a
Roman <roman_zhvakin@mail.ru>
2022-07-23 12:11:51 +0700

    Решение задачи 54 семинар 8

---
+++ HomeW1/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+//двумерного массива.
+
+Console.Clear();
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+RandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив с не упорядоченными значениями");
+string s = new string('-', 11);
+System.Console.WriteLine("" + s);
+PrintArray(numbers);
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1])
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
+    }
+}
+System.Console.WriteLine("" + s);
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+string p = new string('-', 11);
+System.Console.WriteLine("" + p);
+PrintArray(numbers);
+void RandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+System.Console.WriteLine("" + p);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
