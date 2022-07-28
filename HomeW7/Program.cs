38be137f117a134e1c0239386aac6e972c2f4649
Roman <roman_zhvakin@mail.ru>
2022-07-29 01:07:40 +0700

    Решение задания 66 семинар 9

---
+++ HomeW7/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 66: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Clear();
+Console.WriteLine("Введите начальное число M:");
+string s = new string('-', 5);
+System.Console.WriteLine("" + s);
+int numberM = int.Parse(Console.ReadLine());
+System.Console.WriteLine("" + s);
+Console.WriteLine("Введите начальное число N:");
+System.Console.WriteLine("" + s);
+int numberN = int.Parse(Console.ReadLine());
+System.Console.WriteLine("" + s);
+int AckerFunction(int numberM, int numberN)
+{
+    if (numberM == 0)
+        return numberN + 1;
+    if (numberM != 0 && numberN == 0)
+        return AckerFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0)
+        return AckerFunction(numberM - 1, AckerFunction(numberM, numberN - 1));
+    return AckerFunction(numberM, numberN);
+}
+Console.WriteLine(
+    $"Функция Аккермана для чисел A({numberM},{numberN}) = {AckerFunction(numberM, numberN)}");
+Console.WriteLine();
